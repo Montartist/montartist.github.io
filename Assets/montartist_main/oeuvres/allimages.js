d257ee2da131d9fc265f284cc247a9592b368fbc
Svelee <svelee.chee@gmail.com>
2025-03-18 21:08:10 +0100

    Add files via upload
    Unfinished for now

---
+++ Assets/montartist_main/oeuvres/allimages.js
@@ -0,0 +1,58 @@
+import abbaye from './Abbaye de la Sauve Majeur.jpg';
+import amenoashi from './Ame no ashi.jpeg';
+import predestino from './el predestino.jpg';
+import filleLum from './Fille dans la lumière.jpg';
+import Jimmy from './_JIMMY_BUCKETS_.jpg';
+import bear from './_The_Bear_.jpg';
+import golden from './golden.jpeg';
+import ivysm from './Ivysm.jpg';
+import kaneki from './ken kaneki.jpeg';
+import L from './L aime le sucre.jpeg';
+import flamme from './La Flamme.jpg';
+import porte from './La porte.jpg';
+import destin from './Le Destin.jpg';
+import passage from './Le Passage.jpg';
+import pont from './Le pont.jpg';
+import busan from './Le prince de Busan.jpg';
+import tigre from './Le Tigre.jpeg';
+import mago from './Ma Go.jpg';
+import meduse from './Méduses errant dans la mer des pensées.jpeg';
+import miroir from "./miroir d'eau.jpeg";
+import paysage from './Paysage automnal.jpg';
+import pizzia from './Piazza di Ferrari.jpg';
+import prairie from "./Prairie d'été.jpg";
+import princesse from './Princesse de la forêt.jpg';
+import snowxborn from './snowxborn.jpeg';
+import sunkenrock from './Sun ken rock.jpg';
+
+
+const allimages = {
+    abbaye,
+    amenoashi,
+    predestino,
+    filleLum,
+    Jimmy,
+    bear,
+    golden,
+    ivysm,
+    kaneki,
+    L,
+    flamme,
+    porte,
+    destin,
+    passage,
+    pont,
+    busan,
+    tigre,
+    mago,
+    meduse,
+    miroir,
+    paysage,
+    pizzia,
+    prairie,
+    princesse,
+    snowxborn,
+    sunkenrock
+};
+
+export default allimages
